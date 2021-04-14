8b1ed0c296b43657e45718c5561dd0908745e7c5
erikspiegel <82525719+erikspiegel@users.noreply.github.com>
2021-04-14 17:09:36 +0200

    Add files via upload

---
+++ afk_polizei.js
@@ -0,0 +1,120 @@
+registerPlugin({
+    name: 'AFK Polizei auf dem Vetschau TS',
+    version: '1.0',
+    description: 'Kickt Jan',
+    author: 'Erik Spiegel',
+    vars: [
+        {
+            name: 'idleTime',
+            title: 'Max idle time (in minutes)',
+            type: 'number',
+            placeholder: '5',
+            default: 5
+        },
+        {
+            name: 'jan_uid',
+            title: 'UID',
+            type: 'string',
+            placeholder: ''
+        },
+        {
+            name: 'banTime',
+            title: 'Ban length (in minutes)',
+            type: 'number',
+            placeholder: '30',
+            default: 30
+        },
+        {
+            name: 'idleChannelList',
+            title: 'Idle Channel',
+            type: 'array',
+            vars: [
+                {
+                    name: 'idleChannel',
+                    Title: 'Channel',
+                    indent: 1,
+                    type: 'channel'
+                }
+            ]
+        },
+    ]
+}, function(sinusbot, config) {
+
+    const event = require('event')
+    const backend = require('backend')
+
+    const maxIdleTime = config.idleTime * 60 * 1000
+    const UID = config.jan_uid
+    const banTime = config.banTime * 60
+    const idleChannelList = config.idleChannelList.filter(el => el.idleChannel != undefined).map(el => el.idleChannel)
+
+    console.log('Max idle time in minutes : ' + maxIdleTime / 60000)
+    console.log('UID of user: ' + UID)
+    console.log('Ban time in minutes: ' + banTime / 60)
+    console.log('List of idle channels: ' + [... new Set(idleChannelList.map(el => backend.getChannelByID(el).name()))])
+
+    let lastCheckedIdleTime = 0
+    let wasMuted = false
+    let timeOutSet = false
+    let banOnNextConnect = false
+    let timeOut
+
+    event.on('poke', function(ev) {
+        if(timeOutSet && ev.client.uid() === UID) {
+            clearTimeout(timeOut)
+            timeOutSet = false
+            console.log('Timer stopped')
+        }
+    })
+
+    event.on('clientMove', function(ev) {
+        if(ev.fromChannel == undefined && ev.client.uid() === UID && banOnNextConnect) {
+            ev.client.ban(banTime,'Idlemeister')
+            banOnNextConnect = false
+            console.log('Banned on reconnect')
+        }
+    })
+
+
+    function checkAFK() {
+        let client = backend.getClientByUniqueID(UID)
+
+        if(client) {
+
+            let currentChannel = client.getChannels()
+            console.log('Client is in idle channel ' + idleChannelList.includes(currentChannel[0].id()) + ' (' + currentChannel[0].name() + ')')
+
+            if(client.isMuted() || client.isDeaf() || client.isAway()) {
+
+                lastCheckedIdleTime = client.getIdleTime()
+                wasMuted = true
+                console.log('Client is muted')
+
+            } else if(!timeOutSet && !wasMuted && !(idleChannelList.includes(currentChannel[0].id())) && client.getIdleTime() > maxIdleTime) {
+
+                client.poke('AFK-Check bitte den Bot anstupsen')
+
+                timeOutSet = true
+                timeOut = setTimeout(function() {
+
+                    client.kickFromServer('Hör auf zu idlen')
+                    banOnNextConnect = true
+                    console.log('Client didnt poke bot in time')
+
+                },60000)
+                console.log('Timer started')
+
+            } else if(wasMuted && client.getIdleTime() < lastCheckedIdleTime) {
+
+                lastCheckedIdleTime = 0
+                wasMuted = false
+                console.log('Idle time has been reset')
+
+            }
+        } else {
+            console.log('Client isnt on Teamspeak')
+        }
+    }
+
+    setInterval(checkAFK, 120 * 1000)
+});
